ed89d604037ece4b0caf738839ab2d6f330cac10
ColinD13 <downingcolin13@gmail.com>
2026-02-12 17:30:21 -0500

    Adding Timestamp On Form Submission
    Added a time submission for when the contact form is submitted

--- Mod 3/app.js
+++ Mod 3/app.js
@@ -20,18 +20,22 @@ app.get('/contact', (reg,res) => {
     const filePath = path.join(__dirname, "contact.html");
     var html = fs.readFileSync(filePath, "utf8");
     html = html.replace("{{MESSAGE}}", "");
+    html = html.replace("{{SUBMIT_TIME}}", "");
     res.send(html);
 });
 
-//handle post ofr contact form
+//handle post for contact form
 app.post('/contact', (reg, res) =>{
     const name = reg.body.name;
     const message = `Thank you ${name} we have recioeved your message!`;
+    // time submitted
+    const time = "Submitted at " + new Date().toTimeString();
 
     const filePath = path.join(__dirname, "contact.html");
     // had to look this up
     var html = fs.readFileSync(filePath, "utf8");
     html = html.replace("{{MESSAGE}}", message);
+    html = html.replace("{{SUBMIT_TIME}}", time);
     res.send(html);
 })
 
